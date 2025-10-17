bcc39d9d6c0af5de9a4544eff76eca945e126046
hfelices92 <feligo.dev@gmail.com>
2025-10-17 20:29:08 +0200

    @ts-expect-error

--- components/products/ImageUpload.tsx
+++ components/products/ImageUpload.tsx
@@ -12,7 +12,7 @@ export default function ImageUpload({ image }: { image?: string }) {
       onSuccess={(result, { widget }) => {
         if (result.event === "success") {
           widget.close();
-          //@ts-ignore
+          //@ts-expect-error
           setimageUrl(result.info?.secure_url);
         }
       }}
